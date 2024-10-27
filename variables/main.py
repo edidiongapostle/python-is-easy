94048e07a5109af800569a40c75f0005825a367d
edidiongapostle <edidiongapostle@gmail.com>
2024-10-27 03:48:46 +0100

    function assignment on Pirple

---
+++ variables/main.py
@@ -0,0 +1,34 @@
+""" 
+This a short list of my all time favourite songs and some detaila
+about the songs. They are pickeed from different genres as well as
+countries od origin and artists.
+
+"""
+
+Title = "UNSTOPPABLE"
+Artist = "Sia"
+Genre = "POP"
+Duration_In_Secs = "240"
+
+print(Title)
+print(Artist)
+print(Genre)
+print(Duration_In_Secs)
+
+Title = "Ada Ada"
+Artist = "Flavor"
+Genre = "Highlife"
+Duration_In_Secs = "367"
+
+print(Title)
+print(Artist)
+print(Genre)
+print(Duration_In_Secs)
+
+"""
+As time goes, I woould update the list with newer songs that I like as there are endless numbers of them.
+For now, I would stop here to keep up with the course.
+
+Thank you
+
+"""
